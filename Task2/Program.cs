29f8cd5dd1865217d37b9c89794ff2496330ab02
AlisherAbdykhamitov <47111820+AlisherAbdykhamitov@users.noreply.github.com>
2019-02-04 19:21:59 +0600

    Update Program.cs

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,33 +8,35 @@ namespace Task2
 {
     class Student
     {
-        public name;
-        public id;
-        public yearofstudy;
+        public string name;
+        public string id;
+        public string yearofstudy;
+        /* создал 3 стринга (public  - name , id , yearofstudy)*/ 
     
-    public Student(name, id , yearofstudy)
-    {
-        a.name = name;
-        a.id = id;
-        a.yearofstudy = yearofstudy;
-          
-    }
-        public void Getinfo()
+    public Student(string name, string id , string yearofstudy)
         {
-            Cosole.WriteLine(name + " " + id + " " + yearofstudy);
+            this.name = name;
+            this.id = id;
+            this.yearofstudy = yearofstudy;
+                /* "this" это команда , которя присваивает значение к какому либо классу */
         }
-    
+        public void Taken()
+        {
+            Console.WriteLine(name + " " + id + " " + yearofstudy);
+            /* Это у нас функция которые должна выводить нам Имя , айди его , и год учения  */
+        }
+
     }
-    
-    
-    
+
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Student ktk = new Student(Console.ReadLine(), Console.ReadLine(), Console.ReadLine());
-            ktk.Getinfo();
-            
+            Student ktk = new Student(Console.ReadLine(), Console.ReadLine(), Console.ReadLine());/*задаем значения имя, айди и год обучения */
+            ktk.Taken();/*закидываем в функция ,чтоб  вывести все на наш экран*/
+
         }
     }
 }
